a059c7f6276c72565257a0bbda473accb850765f
wendy <wendy@gridwiz.com>
2023-11-10 14:59:26 +0900

    create project

---
+++ websocket_test_v2/src/main/java/com/example/websocket_test_v2/WebsocketTestV2Application.java
@@ -0,0 +1,13 @@
+package com.example.websocket_test_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketTestV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketTestV2Application.class, args);
+    }
+
+}
---
+++ websocket_test_v2/src/test/java/com/example/websocket_test_v2/WebsocketTestV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocket_test_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketTestV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
