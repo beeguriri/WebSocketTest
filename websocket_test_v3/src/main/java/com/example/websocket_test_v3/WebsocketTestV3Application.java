3309dcd7900cd31a104fd43aaa644ba61960daeb
wendy <wendy@gridwiz.com>
2023-11-20 17:42:30 +0900

    create new project

---
+++ websocket_test_v3/src/main/java/com/example/websocket_test_v3/WebsocketTestV3Application.java
@@ -0,0 +1,13 @@
+package com.example.websocket_test_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketTestV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketTestV3Application.class, args);
+	}
+
+}
---
+++ websocket_test_v3/src/test/java/com/example/websocket_test_v3/WebsocketTestV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocket_test_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketTestV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
